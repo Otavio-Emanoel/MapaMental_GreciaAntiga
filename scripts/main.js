10fc113c4ae3a5b6a58b787313db73581ceaea74
Otavio Emanoel <otavioemanuel6b@gmail.com>
2024-11-22 09:17:39 -0300

    Ajuste no Zoom

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,24 @@
 
 function clickMap(event){
-    console.log(event)
-    x = event.x-415;
-    y = event.y - 28
+    // x = event.x-415;
+    // y = event.y - 28
     const mapa = event.target
     if(mapa.classList.contains("zoom_map")){
         mapa.classList.remove("zoom_map")
-        mapa.style.right = "0"
-        mapa.style.bottom  = "0"
+        // mapa.style.right = "0"
+        // mapa.style.bottom  = "0"
     } else {
         mapa.classList.add("zoom_map")
-        console.log(event.x)
-        console.log(event.y)
-        mapa.style.right = x+"px"
-        mapa.style.bottom  = y+"px"
+
+        let imgX = window.innerWidth/2 -600
+        let imgY = window.innerHeight*4/100
+        console.log("Posição %:")
+        console.log((event.x-imgX)*100/1200)
+        console.log((event.y-imgY)*100/984)
+        mapa.style.transformOrigin = `${((event.x-imgX)*100/1200)}% ${((event.y-imgY)*100/984)}%`
+
+
+        // mapa.style.right = x+"px"
+        // mapa.style.bottom  = y+"px"
     }
 }
